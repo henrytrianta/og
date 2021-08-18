23439a63f8e8850747a54ea26a9ac1c5c4bf9436
henrytrianta <henry.trianta@gmail.com>
2021-08-18 20:35:32 +0800

    feat: add member redirect

--- next.config.js
+++ next.config.js
@@ -1,3 +1,31 @@
 module.exports = {
   trailingSlash: false,
+  async redirects() {
+    return [
+      {
+        source: "/member/1",
+        destination:
+          "/api/image?fileType=png&layoutName=Page&Theme=Dark&Title=Henry+Trianta&Subtitle=Swipe+Share+%231",
+        permanent: true,
+      },
+      {
+        source: "/member/2",
+        destination:
+          "/api/image?fileType=png&layoutName=Page&Theme=Dark&Title=Gus+Darma&Subtitle=Swipe+Share+%232",
+        permanent: true,
+      },
+      {
+        source: "/member/3",
+        destination:
+          "/api/image?fileType=png&layoutName=Page&Theme=Dark&Title=Galanggg&Subtitle=Swipe+Share+%233",
+        permanent: true,
+      },
+      {
+        source: "/member/4",
+        destination:
+          "/api/image?fileType=png&layoutName=Page&Theme=Dark&Title=Jayak&Subtitle=Swipe+Share+%234",
+        permanent: true,
+      },
+    ];
+  },
 };
